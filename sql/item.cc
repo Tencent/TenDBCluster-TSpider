213a7835c2ee38f732329d03c99057d0dd12519a
willhan <318467389@qq.com>
2018-01-29 15:44:48 +0800

    ignore send <cache> to remote

--- sql/item.cc
+++ sql/item.cc
@@ -9905,7 +9905,8 @@ void Item_cache::print(String *str, enum_query_type query_type)
     print_value(str);
     return;
   }
-  str->append(STRING_WITH_LEN("<cache>("));
+ // str->append(STRING_WITH_LEN("<cache>("));
+  str->append(STRING_WITH_LEN("/* <cache> */ ("));
   if (example)
     example->print(str, query_type);
   else
