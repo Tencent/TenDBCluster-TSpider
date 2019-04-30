6cb598f167fcb48f8828e3575ee2ea3d04a6d2ef
willhan <willhan@tencent.com>
2019-04-30 17:18:51 +0800

    fix bug for x'' as  hex data;
    -------------------------------
    test case:
    update t_blob set name=x'11223344556677889900' where id='11';

--- sql/item.h
+++ sql/item.h
@@ -4296,6 +4296,7 @@ public:
                         collation.collation);
   }
   void print(String *str, enum_query_type query_type);
+  void print_for_x(String *str, enum_query_type query_type);
   Item *get_copy(THD *thd)
   { return get_item_copy<Item_hex_string>(thd, this); }
 };
